6dbee65fba0fa9c6f2b064eed0cc1817529cb004
Craig Clayton <mancityvsliverpoolfc@outlook.com>
2020-01-20 18:12:00 +0000

    added click event to anchor tags, index.js

--- js/index.js
+++ js/index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+
+const navLink = document.querySelectorAll('.nav-link');
+navLink.forEach(element => {
+    element.addEventListener('click', (event) => {
+        event.target.style.color = 'red';
+        console.log('click')
+    })
+})
