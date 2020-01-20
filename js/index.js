fe2cb9b5edd13e5a0014ca14bcf360da85f9bdbd
Craig Clayton <mancityvsliverpoolfc@outlook.com>
2020-01-20 18:24:51 +0000

    added mouseenter and mouseleave event to images, index.js

--- js/index.js
+++ js/index.js
@@ -1,9 +1,29 @@
 // Your code goes here
 
+// change the anchor tags to red when clicked
 const navLink = document.querySelectorAll('.nav-link');
 navLink.forEach(element => {
     element.addEventListener('click', (event) => {
         event.target.style.color = 'red';
         console.log('click')
+    })
+});
+
+
+// make images go large when mouse enters and leaves
+const img = document.querySelectorAll('img');
+img.forEach(element => {
+    element.addEventListener('mouseenter', (event) => {
+        event.target.style.transform = 'scale(1.2)';
+        event.target.style.transition = 'transform 0.3s'
+        console.log('works')
+    })
+});
+
+// images go back to normal when mouse leaves
+img.forEach(element => {
+    element.addEventListener('mouseleave', (event) => {
+        event.target.style.transform = 'scale(1)';
+        event.target.style.transition = 'transform 0.3s'
     })
 })
